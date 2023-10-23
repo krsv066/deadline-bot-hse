25490cbfac3697bc4010c7ada218444ecbec1c16
Kirill <kagafonov2005@yandex.ru>
2023-10-24 01:48:05 +0300

    version 2.1

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ def remove_from_db(message):
     msg = get_from_db(message)
     key = message.text
     if key in msg:
-        db.remove_from_db(user=message.from_user.id, task=' '.join(msg[key].split()[1:]))
+        db.remove_from_db(user=message.from_user.id, task=msg[key][1])
         bot.send_message(message.chat.id, "Задача успешно удалена")
     else:
         bot.send_message(message.chat.id,
@@ -44,13 +44,13 @@ def get_from_db(message):
     for i in range(len(tasks)):
         date = '-'.join(str(tasks[i]['deadline']).split()[0].split('-')[::-1])
         task = str(tasks[i]['task'])
-        msg[str(i + 1)] = f'{date} {task}'
+        msg[str(i + 1)] = [date, task]
     return msg
 
 
 @bot.message_handler(commands=['start', 'help'])
 def start(message):
-    bot.send_message(message.chat.id, "Инструкция")
+    bot.send_message(message.chat.id, "/help - Подсказка\n/create_deadline - Добавить задачу\n/delete_deadline - Удалить задачу\n/get_deadlines - Посмотреть все задачи\n/cancel - Отмена последнего действия")
 
 
 @bot.message_handler(commands=['cancel'])
@@ -61,7 +61,7 @@ def cancel(message):
 @bot.message_handler(commands=['create_deadline'])
 def message_create_deadline(message):
     msg = bot.send_message(message.chat.id,
-                           'Введите дату дедлайна и задачу через пробел (дата в формате дд-мм-гггг), например:\n29-11-2023 Покормить Барсика')
+                           'Введите дату дедлайна и задачу через пробел (дата в формате дд-мм-гггг), например:\n29-11-2023 Сходить к врачу')
     bot.register_next_step_handler(msg, insert_into_db)
 
 
@@ -70,7 +70,10 @@ def message_delete_deadline(message):
     lst = []
     get_items = get_from_db(message).items()
     for key, value in get_items:
-        lst.append(f'{key}) {value}')
+        lst.append(f'{key}) {" ".join(value)}')
+    if len(lst) == 0:
+        bot.send_message(message.chat.id, 'Пока что нет задач')
+        return
     msg = bot.send_message(message.chat.id, 'Для удаления задачи выберите ее номер:\n\n' + '\n'.join(lst))
     bot.register_next_step_handler(msg, remove_from_db)
 
@@ -80,8 +83,11 @@ def message_get_deadlines(message):
     lst = []
     get_values = get_from_db(message).values()
     for elem in get_values:
-        lst.append(elem)
-    bot.send_message(message.chat.id, 'Ваши задачи (ближайшие дедлайны сверху):\n\n' + '\n'.join(lst))
+        lst.append(' '.join(elem))
+    if len(lst) == 0:
+        bot.send_message(message.chat.id, 'Пока что нет задач')
+    else:
+        bot.send_message(message.chat.id, 'Ваши задачи (ближайшие дедлайны сверху):\n\n' + '\n--------------------------------------\n'.join(lst))
 
 
 bot.infinity_polling()
