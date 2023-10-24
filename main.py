f2af2b8cd4596795b02b4e2bfb44e3d1a21965ee
Kirill <kagafonov2005@yandex.ru>
2023-10-24 09:58:14 +0300

    version 3.0

--- main.py
+++ main.py
@@ -1,93 +1,131 @@
-import db
 import telebot
+from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
+from db import insert_into_db, remove_from_db, get_from_db, make_pretty, clear_db
 from config import TOKEN
 
 bot = telebot.TeleBot(TOKEN)
 
 
-def insert_into_db(message):
-    if message.text == '/cancel':
-        cancel(message)
-        return
-    msg = message.text.split()
-    date_text = '/'.join(msg[0].split('-'))
-    task_text = ' '.join(msg[1:])
+# функция для формирования "менюшки" из кнопок
+def gen_markup(menu="main"):
+    markup = InlineKeyboardMarkup()
+    if menu == "main":  # главное меню ("Добавить дедлайн", "Удалить дедлайн", "Мои дедлайны")
+        markup.row_width = 3
+        markup.add(InlineKeyboardButton("Добавить дедлайн", callback_data="cb_add_deadline"),
+                   InlineKeyboardButton("Удалить дедлайн", callback_data="cb_remove_deadline"),
+                   InlineKeyboardButton("Мои дедлайны", callback_data="cb_get_deadline"))
+
+    if menu == "retry_add":  # меню повторной попытки (для добавления дедлайна)
+        markup.row_width = 2
+        markup.add(InlineKeyboardButton("Попробовать снова", callback_data="cb_retry_add"),
+                   InlineKeyboardButton("Главное меню", callback_data="cb_main_menu"))
+
+    if menu == "retry_remove":  # меню повторной попытки (для удаления дедлайна)
+        markup.row_width = 2
+        markup.add(InlineKeyboardButton("Попробовать снова", callback_data="cb_retry_remove"),
+                   InlineKeyboardButton("Главное меню", callback_data="cb_main_menu"))
+
+    return markup
+
+
+# добавление дедлайна пользователя
+def add_deadline(message):
     user_id = message.from_user.id
+    message = message.text.split()
+    date_str = message[0]
+    task = " ".join(message[1:])
 
     try:
-        db.insert_into_db(user=user_id, date_str=date_text, task=task_text)
-        bot.send_message(message.chat.id, "Задача успешно сохранена")
+        result = insert_into_db(user_id=user_id, date_str=date_str, task=task)
+        bot.send_message(user_id, result, reply_markup=gen_markup())
+
     except Exception:
-        bot.send_message(message.chat.id,
-                         "Что-то пошло не так... Возможные причины ошибки:\n\n1) Такая задача уже существует\n2) Дата введена некорректно\n3) Дата стоит задним числом\n\nПопробуйте снова или используйте команду /cancel для отмены последнего действия")
-        message_create_deadline(message)
+        bot.send_message(user_id, "Упс! Что-то пошло не так", reply_markup=gen_markup(menu="retry_add"))
 
 
-def remove_from_db(message):
-    if message.text == '/cancel':
-        cancel(message)
-        return
-    msg = get_from_db(message)
-    key = message.text
-    if key in msg:
-        db.remove_from_db(user=message.from_user.id, task=msg[key][1])
-        bot.send_message(message.chat.id, "Задача успешно удалена")
-    else:
-        bot.send_message(message.chat.id,
-                         "Что-то пошло не так... Скорее всего ввод некорректен\n\nПопробуйте снова или используйте команду /cancel для отмены последнего действия")
-        message_delete_deadline(message)
+# удаление дедлайна пользователя
+def remove_deadline(message):
+    user_id = message.from_user.id
+    message = message.text
+    ind = int(message.rstrip(")")) - 1
+
+    try:
+        task = get_deadline(user_id=user_id)[ind]["task"]
+        result = remove_from_db(user_id=user_id, task=task)
+        bot.send_message(user_id, result, reply_markup=gen_markup())
+
+    except Exception:
+        bot.send_message(user_id, "Упс! Что-то пошло не так", reply_markup=gen_markup(menu="retry_remove"))
 
 
-def get_from_db(message):
-    tasks = db.get_from_db(user=message.from_user.id)
-    msg = dict()
-    for i in range(len(tasks)):
-        date = '-'.join(str(tasks[i]['deadline']).split()[0].split('-')[::-1])
-        task = str(tasks[i]['task'])
-        msg[str(i + 1)] = [date, task]
-    return msg
+# получение всех дедлайнов пользователя
+def get_deadline(user_id):
+    return get_from_db(user_id=user_id)
 
 
-@bot.message_handler(commands=['start', 'help'])
-def start(message):
-    bot.send_message(message.chat.id, "/help - Подсказка\n/create_deadline - Добавить задачу\n/delete_deadline - Удалить задачу\n/get_deadlines - Посмотреть все задачи\n/cancel - Отмена последнего действия")
+# приветствие бота, команды "/start" и "/help"
+@bot.message_handler(commands=["help", "start"])
+def send_welcome(message):
+    bot.reply_to(message, """\
+Привет, я Deadly.
+Я могу хранить твои задачи и дедлайны! Ну что, начнем?\
+""", reply_markup=gen_markup())
 
 
-@bot.message_handler(commands=['cancel'])
-def cancel(message):
-    bot.send_message(message.chat.id, "Отмена последнего действия")
+# команда "/clear" (удаление всех дедлайнов пользователя)
+@bot.message_handler(commands=["clear"])
+def clear_deadlines(message):
+    user_id = message.from_user.id
+    result = clear_db(user_id=user_id)
+    bot.reply_to(message, result, reply_markup=gen_markup())
 
 
-@bot.message_handler(commands=['create_deadline'])
-def message_create_deadline(message):
-    msg = bot.send_message(message.chat.id,
-                           'Введите дату дедлайна и задачу через пробел (дата в формате дд-мм-гггг), например:\n29-11-2023 Сходить к врачу')
-    bot.register_next_step_handler(msg, insert_into_db)
+# обработка кнопок из меню
+@bot.callback_query_handler(func=lambda call: True)
+def callback_query(call):
+    user_id = call.from_user.id
+
+    if call.data == "cb_add_deadline":  # кнопка Добавить дедлайн
+        bot.answer_callback_query(call.id, "")
+        msg = bot.send_message(user_id,
+                               "Введи дату дедлайна и задачу через пробел (дата в формате дд-мм-гггг), например:\n6-11-2023 Сделать ИДЗ-3 по линалу")
+        bot.register_next_step_handler(msg, add_deadline)
+
+    elif call.data == "cb_remove_deadline":  # кнопка Удалить дедлайн
+        bot.answer_callback_query(call.id, "")
+        rows = get_deadline(user_id=user_id)
+        if rows:
+            text = make_pretty(rows)
+            msg = bot.send_message(user_id, "Выбери номер дедлайна, который хочешь удалить:\n" + text)
+            bot.register_next_step_handler(msg, remove_deadline)
+        else:
+            bot.send_message(user_id, "У тебя пока нет дедлайнов", reply_markup=gen_markup())
+
+    elif call.data == "cb_get_deadline":  # кнопка Мои дедлайны
+        bot.answer_callback_query(call.id, "")
+        rows = get_deadline(user_id=user_id)
+        if rows:
+            text = make_pretty(rows)
+            bot.send_message(user_id, text, reply_markup=gen_markup())
+        else:
+            bot.send_message(user_id, "У тебя пока нет дедлайнов", reply_markup=gen_markup())
+
+    elif call.data == "cb_retry_add":  # кнопка Попробовать снова (для добавления)
+        bot.answer_callback_query(call.id, "")
+        msg = bot.send_message(user_id,
+                               "Введи дату дедлайна и задачу через пробел (дата в формате дд-мм-гггг), например:\n6-11-2023 Сделать ИДЗ-3 по линалу")
+        bot.register_next_step_handler(msg, add_deadline)
+
+    elif call.data == "cb_retry_remove":  # кнопка Попробовать снова (для удаления)
+        bot.answer_callback_query(call.id, "")
+        rows = get_deadline(user_id=user_id)
+        text = make_pretty(rows)
+        msg = bot.send_message(user_id, text)
+        bot.register_next_step_handler(msg, remove_deadline)
+
+    elif call.data == "cb_main_menu":  # кнопка Главное меню
+        bot.answer_callback_query(call.id, "")
+        bot.send_message(user_id, "Чем могу помочь?", reply_markup=gen_markup())
 
 
-@bot.message_handler(commands=['delete_deadline'])
-def message_delete_deadline(message):
-    lst = []
-    get_items = get_from_db(message).items()
-    for key, value in get_items:
-        lst.append(f'{key}) {" ".join(value)}')
-    if len(lst) == 0:
-        bot.send_message(message.chat.id, 'Пока что нет задач')
-        return
-    msg = bot.send_message(message.chat.id, 'Для удаления задачи выберите ее номер:\n\n' + '\n'.join(lst))
-    bot.register_next_step_handler(msg, remove_from_db)
-
-
-@bot.message_handler(commands=['get_deadlines'])
-def message_get_deadlines(message):
-    lst = []
-    get_values = get_from_db(message).values()
-    for elem in get_values:
-        lst.append(' '.join(elem))
-    if len(lst) == 0:
-        bot.send_message(message.chat.id, 'Пока что нет задач')
-    else:
-        bot.send_message(message.chat.id, 'Ваши задачи (ближайшие дедлайны сверху):\n\n' + '\n--------------------------------------\n'.join(lst))
-
-
-bot.infinity_polling()
+bot.infinity_polling()  # запуск бота
